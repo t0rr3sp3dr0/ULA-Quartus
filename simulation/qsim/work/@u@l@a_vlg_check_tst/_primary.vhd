2675045e86cc0374d1a94cb8f1ef4816aae487e9
Pedro Henrique Torres Santos <phts@ug4c17.windows.cin.ufpe.br>
2016-10-26 21:19:31 -0300

    final ULA

---
+++ simulation/qsim/work/@u@l@a_vlg_check_tst/_primary.vhd
@@ -0,0 +1,10 @@
+library verilog;
+use verilog.vl_types.all;
+entity ULA_vlg_check_tst is
+    port(
+        F               : in     vl_logic_vector(3 downto 0);
+        overflow        : in     vl_logic;
+        status          : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end ULA_vlg_check_tst;
